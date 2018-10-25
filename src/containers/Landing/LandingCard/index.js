983d8988445866ebf2ee71b188623eea8d2f6fee
muticansel <cancel.muti@gmail.com>
2018-10-25 22:04:16 +0300

    Landing page styling

--- src/containers/Landing/LandingCard/index.js
+++ src/containers/Landing/LandingCard/index.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
+import { CardContent, ListSubheader } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
+import Ionicon from 'react-ionicons';
 import './style.css';
 
 class LandingCard extends Component {
@@ -16,27 +15,30 @@ class LandingCard extends Component {
         const { classes } = this.props;
 
         return (
-            <Card className={classes.content} onClick={this.props.clicked}>
-                <CardContent>
-                    {this.props.name}
+            <Card className={classes.card} onClick={this.props.clicked}>
+                {this.props.name === 'EVLER' ? <Ionicon icon="ios-home-outline"  style={{height: '20%', width: '30%'}} color="white" /> : <Ionicon icon="ios-people-outline" style={{height: '20%', width: '30%'}} color="white" />}
+                <ListSubheader component="div" style={{ fontSize: '25px', color: 'white' }}>{this.props.name}</ListSubheader>
+                <CardContent style={{ height: '75%' }}>
+                    {this.props.content}
                 </CardContent>
-                <CardActions>
-                    <Button size="small">Learn More</Button>
-                </CardActions>
             </Card >
         )
     }
 }
 
 const style = theme => ({
-    content: {
+    card: {
         width: '25%',
+        height: '80%',
         margin: '1em',
         textAlign: 'center',
-        backgroundColor: '#8C089F',
+        backgroundColor: '#9F009F',
         color: '#f3f3f3',
-        cursor: 'pointer'
-    }
+        cursor: 'pointer',
+        borderRadius: '15px',
+        border: '2px solid #6C046C'
+    },
+
 })
 
 export default withStyles(style)(LandingCard);
--- src/containers/Landing/index.js
+++ src/containers/Landing/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import LandingCard from './LandingCard';
-import './LandingCard/style.css'
+import { withStyles } from '@material-ui/core/styles';
+import './LandingCard/style.css';
 
 class Landing extends Component {
     panelClicked = (path) => {
@@ -8,13 +9,30 @@ class Landing extends Component {
     }
 
     render() {
+        const { classes } = this.props;
         return (
-            <div className="Landing">
-                <LandingCard name="EVLER" clicked={(path) => this.panelClicked('/houses')} />
-                <LandingCard name="KİŞİLER" clicked={(path) => this.panelClicked('/roommates')} />
+            <div className={classes.landing}>
+                <LandingCard name="EVLER" clicked={(path) => this.panelClicked('/houses')} content={content.home} />
+                <LandingCard name="KİŞİLER" clicked={(path) => this.panelClicked('/roommates')} content={content.roommates}/>
             </div>
         )
     }
 }
 
-export default Landing;
+const content = {
+    home: 'Hemen seçebileceğin yüzlerce ev ilanına göz at ve ideal ev arkadaşınla yaşamaya başla',
+    roommates: 'Beraber eve çıkabileceğin, tam da aradığın kişiler ile iletişime geç ve hayalindeki evi kur'
+}
+
+const style = theme => ({
+    landing: {
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'center' ,
+        justifyContent: 'center',
+        flex: '1',
+        backgroundColor: '#4A004A'
+    }
+})
+
+export default withStyles(style)(Landing);
