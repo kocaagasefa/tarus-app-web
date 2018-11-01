5dab676fd8e372f1240a3f96f0bb570c9702755b
muticansel <cancel.muti@gmail.com>
2018-11-01 23:12:51 +0300

    keys.js added

---
+++ src/config/keys.js
@@ -0,0 +1,8 @@
+export const FirebaseConfig = {
+  apiKey: "AIzaSyAwF9D75357QK5bYIK72K_Tn1l3Gmr70O4",
+  authDomain: "homebineapp-8f4dd.firebaseapp.com",
+  databaseURL: "https://homebineapp-8f4dd.firebaseio.com",
+  projectId: "homebineapp-8f4dd",
+  storageBucket: "homebineapp-8f4dd.appspot.com",
+  messagingSenderId: "938331350307"
+};
