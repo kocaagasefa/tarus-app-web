1eabb591171204e35d1c040239851b302c80ba6a
kocaagasefa <kocaagasefa@gmail.com>
2018-11-01 23:16:57 +0300

    "keys added"

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,11 +1,5 @@
 
 export const FirebaseConfig = {
-    // apiKey: "AIzaSyAiD9DNasjLsZ0p5w3bE94G_aGPDNmbIvI",
-    // authDomain: "evarkadasim-1.firebaseapp.com",
-    // databaseURL: "https://evarkadasim-1.firebaseio.com",
-    // projectId: "evarkadasim-1",
-    // storageBucket: "evarkadasim-1.appspot.com",
-    // messagingSenderId: "566945460977"//
     apiKey: "AIzaSyAwF9D75357QK5bYIK72K_Tn1l3Gmr70O4",
     authDomain: "homebineapp-8f4dd.firebaseapp.com",
     databaseURL: "https://homebineapp-8f4dd.firebaseio.com",
