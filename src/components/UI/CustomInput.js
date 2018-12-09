7f98404ef5732544758acc8abbdce58304604f06
kocaagasefa <kocaagasefa@gmail.com>
2018-12-09 23:06:20 +0300

    props added to Custom Input

--- src/components/UI/CustomInput.js
+++ src/components/UI/CustomInput.js
@@ -9,6 +9,7 @@ const customInput = props => {
     <div className={props.classes.container} style={props.containerStyle}>
         {props.lefticon}
         <InputBase  
+            {...props}
             className={[props.classes.input,props.lefticon&&props.classes.inputWithIcon]}
             style={props.style}/>
     </div>
@@ -18,7 +19,7 @@ const style = theme => ({
     container:{
         border:"1px solid #9927B1",
         borderRadius:theme.spacing.unit,
-        backgroundColor:"#452051",
+        backgroundColor:"rgba(255,255,255,.1)",
         padding:theme.spacing.unit,
         display:"flex",
         alignItems:"center",
@@ -31,10 +32,10 @@ const style = theme => ({
     },
     inputWithIcon:{
         paddingLeft:theme.spacing.unit,
-        borderLeft:`1px solid white`,
+        borderLeft:`1px solid #9927B1`,
         marginLeft:theme.spacing.unit
     }
 
-})
+});
 
 export default withStyles(style)(customInput)
