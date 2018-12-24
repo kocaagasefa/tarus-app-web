de85dce39a4f600c458a88da9e4925e8227d5ab5
Cansel Muti <cancel.muti@gmail.com>
2018-12-24 15:25:54 +0300

    Navbar - Signout button styling

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,43 +1,39 @@
 import React from 'react';
-import { withStyles, FormLabel, Chip,AppBar, Toolbar, IconButton, Typography, Button } from '@material-ui/core';
+import { withStyles, Chip, AppBar, Toolbar, IconButton, Typography, Button } from '@material-ui/core';
 import { Menu, ClassSharp } from '@material-ui/icons';
 
 const navBar = props => {
-    const { classes,t, toggleDrawer } = props;
+    const { classes, t, toggleDrawer } = props;
 
     return (
         <div className={classes.navBar}>
             <AppBar position="relative" className={classes.navBar}>
-            <Toolbar>
-            <IconButton className={ClassSharp.menuButton} color="inherit" onClick={toggleDrawer}>
-                    <Menu />
-                </IconButton>
-                <Typography className={classes.brand} variant="h6" color="inherit">
-                    HomeBine
+                <Toolbar>
+                    <IconButton className={ClassSharp.menuButton} color="inherit" onClick={toggleDrawer}>
+                        <Menu />
+                    </IconButton>
+                    <Typography className={classes.brand} variant="h6" color="inherit">
+                        HomeBine
                 </Typography>
-                <div>
-                {
-                props.user ?
-                    <>
-                        <Chip className={classes.content} label={props.user.email}></Chip>
-                        <FormLabel className={classes.content} onClick={props.signOut}>{t('navbar.signout')}</FormLabel>
-                    </>
-                    :
-                    <>
-                        <Button onClick={props.onSignInOpen} className={classes.content} color="inherit">{t('navbar.signin')}</Button>
-                        
-                        <Button onClick={props.onSignUpOpen} className={classes.content} color="inherit">{t('navbar.signup')}</Button>
-                    </>
-            }
+                    <div>
+                        {
+                            props.user ?
+                                <>
+                                    <Chip className={classes.content} label={props.user.email}></Chip>
+                                    <Button className={classes.content} onClick={props.signOut} color="inherit">{t('navbar.signout')}</Button>
+                                </>
+                                :
+                                <>
+                                    <Button onClick={props.onSignInOpen} className={classes.content} color="inherit">{t('navbar.signin')}</Button>
+                                    <Button onClick={props.onSignUpOpen} className={classes.content} color="inherit">{t('navbar.signup')}</Button>
+                                </>
+                        }
 
-                </div>
-                
-
-                        <Button color="inherit" className={classes.content+" "+ (props.lng==="en"?classes.activeLanguage:"")} onClick={()=>props.changeLanguage('en')}> EN </Button>
-                        
-                        <Button color="inherit" className={classes.content+" "+(props.lng==="tr"?classes.activeLanguage:"")} onClick={()=>props.changeLanguage('tr')}> TR </Button>
-                        </Toolbar>
-                        </AppBar>
+                    </div>
+                    <Button color="inherit" className={classes.content + " " + (props.lng === "en" ? classes.activeLanguage : "")} onClick={() => props.changeLanguage('en')}> EN </Button>
+                    <Button color="inherit" className={classes.content + " " + (props.lng === "tr" ? classes.activeLanguage : "")} onClick={() => props.changeLanguage('tr')}> TR </Button>
+                </Toolbar>
+            </AppBar>
         </div>
     )
 };
@@ -46,21 +42,21 @@ const navBar = props => {
 
 const styles = theme => ({
     navBar: {
-        flexGrow:1,
+        flexGrow: 1,
         backgroundImage: 'linear-gradient(-180deg, #380040 0%, #6F007F 100%)',
         zIndex: theme.zIndex.drawer + 1,
     },
-    brand:{
-        flexGrow:1
+    brand: {
+        flexGrow: 1
     },
-    activeLanguage:{
-        fontWeight:"bold"
+    activeLanguage: {
+        fontWeight: "bold"
     },
 
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
-  },
+    menuButton: {
+        marginLeft: -12,
+        marginRight: 20,
+    },
 })
 
-export default withStyles(styles)( navBar);
+export default withStyles(styles)(navBar);
